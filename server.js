2dd2eaad8f6f5440c20508c3627e389b9ed3a5a2
Sourabh Kumar <inspiron3593_8MT6S53@outlook.com>
2023-07-18 13:08:59 +0530

    Configured sequelize using cli

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express'); // package to get the express object constructor
 const bodyParser = require('body-parser');
+const fs = require('fs');
+require('dotenv').config();
 
 const app = express(); // This function returns an express object ehich has the capabilities to handle server side requests
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const PORT = 3000; //this will be the port on our local system where server will run
+const PORT = process.env.PORT; //this will be the port on our local system where server will run
+
+let content = JSON.parse(fs.readFileSync('config/config.json','utf-8'));
+content.development.password = process.env.DB_PASS;
+fs.writeFileSync('config/config.json',JSON.stringify(content));
 
 app.listen(PORT,()=>{ //listen function register the app for the port
     console.log(`Server started on Port ${PORT}`);
