9b9699ecb2b85251a83d772daf0a4887cccad2b1
Sourabh Kumar <inspiron3593_8MT6S53@outlook.com>
2023-07-19 22:35:40 +0530

    Feature: Added create category api

---
+++ services/category.service.js
@@ -0,0 +1,20 @@
+// const {Category} = require('../models/index');
+const Category = require('../models/index').Category;
+
+const create = async( data ) => {
+    try{
+        const category = await Category.create({
+            name: data.name,
+            description: data.description
+        });
+        return category;
+    }
+    catch(err) {
+        console.log("Something went wrong");
+        console.log(err);
+    }
+}
+
+module.exports = {
+    create
+}
