a2801820675bf05304ea3cfe9e31c2458f68eb01
Sourabh Kumar <inspiron3593_8MT6S53@outlook.com>
2023-08-14 13:01:45 +0530

    Feature: deletion api for product added

--- middlewares/product.validator.js
+++ middlewares/product.validator.js
@@ -2,7 +2,7 @@ const productCreateValidator = (req,res,next)=> {
     if(!req.body.name || !req.body.cost) {
         return res.status(400).json({
             message: 'Invalid request body',
-            success: true,
+            success: false,
             data: {},
             err: 'Either name or cost is missing from the request object'
         });
@@ -10,6 +10,32 @@ const productCreateValidator = (req,res,next)=> {
     next(); 
 }
 
+const productUpdateValidater = (req,res,next)=> {
+    if(!req.body.name || !req.body.description || !req.body.cost) {
+        return res.status(400).json({
+            message: 'Invalid request body',
+            success: false,
+            data: {},
+            err: 'Either name, description or cost is missing from the request object'
+        });
+    }
+    next();
+}
+
+const productPatchValidator = (req,res,next) => {
+    if(!(req.body.name || req.body.description || req.body.cost)) {
+        return res.status(400).json({
+            message: 'Invalid request body',
+            success: false,
+            data: {},
+            err: 'At least one parameter among cost, name and description is expected'     
+        })
+    }
+    next();
+}
+
 module.exports = {
-    productCreateValidator   
+    productCreateValidator ,
+    productUpdateValidater ,
+    productPatchValidator 
 }
